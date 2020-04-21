2bed15c221cc982c384c17c147b8eb2ff74d8a93
Shai Gerbi <ShaiGerbi@gmail.com>
2020-04-21 20:09:25 +0300

    Initial dist subtree commit

---
+++ precache-manifest.0a381eb7d58a6f1333d733185cfd7cf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9081a96769661067d2be1c05088bfcc",
+    "url": "/DutyCycle/index.html"
+  },
+  {
+    "revision": "1373f05dc24010183658",
+    "url": "/DutyCycle/js/app.d56d95d2.js"
+  },
+  {
+    "revision": "e0eb4205186f07175387",
+    "url": "/DutyCycle/js/chunk-vendors.a44191cd.js"
+  },
+  {
+    "revision": "b7cccad5037efc5842f987927ad7e319",
+    "url": "/DutyCycle/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/DutyCycle/robots.txt"
+  }
+]);
